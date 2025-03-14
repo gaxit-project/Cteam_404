7c2b793d64bd99ded0687e495b93c5a6a60acc00
e1b24097 <e1b24097@oit.ac.jp>
2025-03-15 01:43:58 +0900

    Revert "Revert"
    This reverts commit 7d18ae277b52e033fb7698561a75e2a31d13ac49.

--- Assets/Project/Scenes/SceneTest/IoriTest/BossSpecialAttackFront.cs
+++ Assets/Project/Scenes/SceneTest/IoriTest/BossSpecialAttackFront.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine.UIElements;
 
-public class BossSpecialAttack : MonoBehaviour
+public class BossSpecialAttackFront : MonoBehaviour
 {
     [SerializeField] private ParticleSystem specialEffect;
     [SerializeField] private Collider attackCollider;
@@ -13,15 +15,31 @@ public class BossSpecialAttack : MonoBehaviour
     [SerializeField] private float sideOffset = 0f;//正：右, 負：左）
 
     private BossStateAI bossStateAI;
+    private Rigidbody attackRigidBody;
     private bool isAttacking = false; // 攻撃中かどうかを判定
 
+    [Header("トンネリング対策")]
+    [SerializeField] private float _colliderScaleFactor = 1.2f;
+    [SerializeField] private float maxSpeed = 10f;
     private void Start()
     {
         bossStateAI = FindObjectOfType<BossStateAI>();
         if (attackCollider != null)
         {
             attackCollider.enabled = false;
+
+            attackRigidBody = attackCollider.GetComponent<Rigidbody>();
+            if(attackRigidBody == null)
+            {
+                attackRigidBody = attackCollider.gameObject.AddComponent<Rigidbody>();
+            }
+
+            attackRigidBody.useGravity = false;
+            attackRigidBody.isKinematic = true;
+            attackRigidBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
         }
+
+        AdjustColiderSize();
     }
 
     private void Update()
@@ -60,6 +78,12 @@ public class BossSpecialAttack : MonoBehaviour
         //firePoint の方向を攻撃の向きに設定
         Quaternion attackRotation = firePoint.rotation;
 
+        //RigidBodyの速度を制限
+        if(attackRigidBody != null)
+        {
+            Vector3 velocity = (attackPos - attackCollider.transform.position) / Time.deltaTime;
+            attackRigidBody.linearVelocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        }
         //攻撃の位置と向きをリアルタイム更新
         if (attackCollider != null)
         {
@@ -98,17 +122,60 @@ public class BossSpecialAttack : MonoBehaviour
             bossStateAI.SpecialAttackFinished();
         }
     }
-
+    /// <summary>
+    /// コライダーのサイズを調整して、トンネリングを防ぐ
+    /// </summary>
+    private void AdjustColiderSize()
+    {
+        BoxCollider boxCollider = attackCollider as BoxCollider;
+        if(boxCollider != null)
+        {
+            boxCollider.size *= _colliderScaleFactor;
+        }
+    }   
     // パーティクルのコライダーにプレイヤーが触れたときの処理
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Triggerでダメージ");
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage();
+                
+            }
+            else
+            {
+                Debug.Log("PlayerHealthがないよ");
             }
         }
+        else
+        {
+            Debug.Log("PlayerTagがないよ");
+        }
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage();
+                Debug.Log("Collitionでダメージ");
+            }
+            else
+            {
+                Debug.Log("PlayerHealthがないよ");
+            }
+        }
+        else
+        {
+            Debug.Log("PlayerTagがないよ");
+        }
+    }
+
+
 }
