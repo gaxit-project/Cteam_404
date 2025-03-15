5b89b57e4df338022d506292aedba5392d870fd8
jaga-Nick <shrimp1059yt@gmail.com>
2025-03-15 10:16:14 +0900

    ty

--- Assets/Project/Script/MainScene/PauseManager.cs
+++ Assets/Project/Script/MainScene/PauseManager.cs
@@ -26,6 +26,12 @@ public class PauseManager : MonoBehaviour
         BuildIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
+    private void Start()
+    {
+        Canvas = GameObject.Find("Canvas");  //Canvasを取得
+        Canvas.SetActive(false);  //取得してから非アクティブに変更
+    }
+
 
     #region Sceneを移動したときの処理
     private void OnEnable()
